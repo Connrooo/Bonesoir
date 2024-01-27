66d6d9c3c0defc117818e0955deb2a0ea50d9c1e
Connrooo <ccconnor.robinsonnn@gmail.com>
2024-01-27 21:13:20 +0000

    added gold

--- Catacombs/Assets/Scripts/Player/PlayerMotion.cs
+++ Catacombs/Assets/Scripts/Player/PlayerMotion.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
+using UnityEditor;
 
 public class PlayerMotion : MonoBehaviour
 {
